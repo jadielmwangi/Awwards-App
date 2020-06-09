156983057ef229aed17074afb17303958eb13ae8
jadielmwangi <jedielmwangi@gmail.com>
2020-06-09 08:52:19 +0300

    adds profileTestclass for testing

--- awward/tests.py
+++ awward/tests.py
@@ -1,1 +1,29 @@
 
+from django.test import TestCase
+from .models import Profile
+
+# Create your tests here.
+
+class ProfileTestClass(TestCase):
+    #Set up method
+
+    def setUp(self):
+        self.new_user = User(username='jadiel', email='jedielmwangi@gmail.com', password='1234')
+        self.new_user.save()
+        self.new_profile = Profile(user=self.new_user,profile_picture="image.jpeg",bio="just testing", contact='jedielmwangi@gmail.com')
+    
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,Profile))
+    
+    def test_save_method(self):
+        self.new_profile.save_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)>0)
+
+    def test_delete_method(self):
+        self.new_profile.save_profile()
+        self.new_profile.delete_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)==0)   
+
